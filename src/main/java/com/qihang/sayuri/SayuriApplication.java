546dc42801c2c43d4348fc0b6b0c2f08f5404336
qihang <1332587753@qq.com>
2022-06-17 16:39:32 +0800

    Initial commit

---
+++ src/main/java/com/qihang/sayuri/SayuriApplication.java
@@ -0,0 +1,13 @@
+package com.qihang.sayuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayuriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SayuriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qihang/sayuri/SayuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qihang.sayuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SayuriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
